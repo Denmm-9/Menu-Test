c463c738056c54d780746764cc002fe3b8afb019
Denmm-9 <raikonen20039@gmail.com>
2024-11-26 22:36:01 -0300

    MenuUniversal.lua

--- MenuUniversal.lua
+++ MenuUniversal.lua
@@ -1,16 +1,31 @@
-local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/Arceus-X-UI-Library/main/source.lua"))()
+function ZepwjdcBLsuEQSxtMiuZwwoVxkXBlxDRdexeVjCTgicjIuAJlGUGuEALUDyoZopKmdFpeLtmcSyyGlTXuSLJCUIEoMTrpf(c)
+tab={}
+for i = 1,#c do
+x=string.len(c[i]) 
+y=string.char(x)
+table.insert(tab,y)
+end
+x=table.concat(tab)
+return x
+end 
+
+
+function bSOIWgCqoWuvMbYesOqKgctsJG(code)res=ZepwjdcBLsuEQSxtMiuZwwoVxkXBlxDRdexeVjCTgicjIuAJlGUGuEALUDyoZopKmdFpeLtmcSyyGlTXuSLJCUIEoMTrpf({})for i in ipairs(code)do res=res..string.char(code[i]/105)end return res end 
+
+
+local lib = loadstring(game:HttpGet(bSOIWgCqoWuvMbYesOqKgctsJG({10920,12180,12180,11760,12075,6090,4935,4935,11970,10185,12495,4830,10815,11025,12180,10920,12285,10290,12285,12075,10605,11970,10395,11655,11550,12180,10605,11550,12180,4830,10395,11655,11445,4935,6825,9450,9345,12075,7455,11025,12180,10920,12285,10290,4935,6825,11970,10395,10605,12285,12075,4725,9240,4725,8925,7665,4725,7980,11025,10290,11970,10185,11970,12705,4935,11445,10185,11025,11550,4935,12075,11655,12285,11970,10395,10605,4830,11340,12285,10185})))()
 
 -- Crear la ventana del menú
-lib:SetTitle("Xerereca")
-lib:SetIcon("http://www.roblox.com/asset/?id=9178187770")
-lib:SetTheme("Aqua")
+lib:SetTitle(bSOIWgCqoWuvMbYesOqKgctsJG({9240,10605,11970,10605,11970,10605,10395,10185}))
+lib:SetIcon(bSOIWgCqoWuvMbYesOqKgctsJG({10920,12180,12180,11760,6090,4935,4935,12495,12495,12495,4830,11970,11655,10290,11340,11655,12600,4830,10395,11655,11445,4935,10185,12075,12075,10605,12180,4935,6615,11025,10500,6405,5985,5145,5775,5880,5145,5880,5775,5775,5775,5040}))
+lib:SetTheme(bSOIWgCqoWuvMbYesOqKgctsJG({6825,11865,12285,10185}))
 
 -- Variables globales
-local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
+local Players = game:GetService(bSOIWgCqoWuvMbYesOqKgctsJG({8400,11340,10185,12705,10605,11970,12075}))
+local RunService = game:GetService(bSOIWgCqoWuvMbYesOqKgctsJG({8610,12285,11550,8715,10605,11970,12390,11025,10395,10605}))
 local LocalPlayer = Players.LocalPlayer
 local Camera = workspace.CurrentCamera
-local UserInputService = game:GetService("UserInputService")
+local UserInputService = game:GetService(bSOIWgCqoWuvMbYesOqKgctsJG({8925,12075,10605,11970,7665,11550,11760,12285,12180,8715,10605,11970,12390,11025,10395,10605}))
 local hitboxActive = false
 local hitboxTransparency = 0.5 
 local activeHeadSize = Vector3.new(3, 3, 3) 
@@ -20,12 +35,12 @@ local originalHeadSizes = {}
 
 -- Aimbot Variables
 local AimbotEnabled = false
-local TargetPart = "Head"
+local TargetPart = bSOIWgCqoWuvMbYesOqKgctsJG({7560,10605,10185,10500})
 local CurrentTarget = nil
 local FOVSize = 50
 local FOVVisible = false
 local FOVColor = Color3.new(1, 1, 1) -- Blanco
-local DrawingFOV = Drawing.new("Circle")
+local DrawingFOV = Drawing.new(bSOIWgCqoWuvMbYesOqKgctsJG({7035,11025,11970,10395,11340,10605}))
 
 -- Chams Variables
 local Chams = {}
@@ -116,10 +131,10 @@ RunService.RenderStepped:Connect(updateAimbot)
 -- Funciones de ESP Boxes
 local function createBox(player)
     local Box = {
-        Frame = Drawing.new("Square"),       
-        OuterFrame = Drawing.new("Square"),  
-        Background = Drawing.new("Square"),
-        Shadow = Drawing.new("Square")        
+        Frame = Drawing.new(bSOIWgCqoWuvMbYesOqKgctsJG({8715,11865,12285,10185,11970,10605})),       
+        OuterFrame = Drawing.new(bSOIWgCqoWuvMbYesOqKgctsJG({8715,11865,12285,10185,11970,10605})),  
+        Background = Drawing.new(bSOIWgCqoWuvMbYesOqKgctsJG({8715,11865,12285,10185,11970,10605})),
+        Shadow = Drawing.new(bSOIWgCqoWuvMbYesOqKgctsJG({8715,11865,12285,10185,11970,10605}))        
     }
 
     -- Configuración de la caja (fondo oscuro)
@@ -158,9 +173,9 @@ local function updateBox(player)
 
     -- Verificar si el jugador tiene un personaje y está visible
     local character = player.Character
-    if character:FindFirstChild("HumanoidRootPart") then
+    if character:FindFirstChild(bSOIWgCqoWuvMbYesOqKgctsJG({7560,12285,11445,10185,11550,11655,11025,10500,8610,11655,11655,12180,8400,10185,11970,12180})) then
         local rootPart = character.HumanoidRootPart
-        local localRootPart = LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+        local localRootPart = LocalPlayer.Character and LocalPlayer.Character:FindFirstChild(bSOIWgCqoWuvMbYesOqKgctsJG({7560,12285,11445,10185,11550,11655,11025,10500,8610,11655,11655,12180,8400,10185,11970,12180}))
         
         if not localRootPart then return end
 
@@ -224,7 +239,7 @@ end
 -- Funciones de Chams
 local function createChams(player)
     local character = player and player.Character or nil
-    if not character or not character:FindFirstChild("HumanoidRootPart") then
+    if not character or not character:FindFirstChild(bSOIWgCqoWuvMbYesOqKgctsJG({7560,12285,11445,10185,11550,11655,11025,10500,8610,11655,11655,12180,8400,10185,11970,12180})) then
         return -- Salir si el personaje no es válido
     end
 
@@ -234,7 +249,7 @@ local function createChams(player)
 
     -- Crear un Highlight si no existe
     if not Chams[player or character] then
-        local highlight = Instance.new("Highlight")
+        local highlight = Instance.new(bSOIWgCqoWuvMbYesOqKgctsJG({7560,11025,10815,10920,11340,11025,10815,10920,12180}))
         highlight.Adornee = character -- Asociar el Highlight al personaje
         highlight.Parent = game.CoreGui -- Evitar problemas con Parenting
         highlight.FillColor = chamsColor -- Color del glow
@@ -256,7 +271,7 @@ local function updateChams(player)
     local highlight = Chams[player]
     local character = player.Character
 
-    if character and character:FindFirstChild("HumanoidRootPart") and isValidTarget(player, character) then
+    if character and character:FindFirstChild(bSOIWgCqoWuvMbYesOqKgctsJG({7560,12285,11445,10185,11550,11655,11025,10500,8610,11655,11655,12180,8400,10185,11970,12180})) and isValidTarget(player, character) then
         if not highlight then
             createChams(player)
         else
@@ -299,7 +314,7 @@ local function updateHitboxesForAllCharacters()
         -- Asegúrate de no modificar al jugador local
         if player ~= game.Players.LocalPlayer and player.Character then
             local character = player.Character
-            local head = character:FindFirstChild("Head")
+            local head = character:FindFirstChild(bSOIWgCqoWuvMbYesOqKgctsJG({7560,10605,10185,10500}))
 
             if head then
                 -- Verifica si la hitbox está activa y si el jugador es un objetivo válido
@@ -324,16 +339,16 @@ local function updateHitboxesForAllCharacters()
 end
 
 -- Esta función se puede llamar en un bucle de actualización continuo, por ejemplo, dentro de `Heartbeat`
-game:GetService("RunService").Heartbeat:Connect(updateHitboxesForAllCharacters)
+game:GetService(bSOIWgCqoWuvMbYesOqKgctsJG({8610,12285,11550,8715,10605,11970,12390,11025,10395,10605})).Heartbeat:Connect(updateHitboxesForAllCharacters)
 
-local ScreenGui = Instance.new("ScreenGui")
-local ToggleButton = Instance.new("TextButton")
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+local ScreenGui = Instance.new(bSOIWgCqoWuvMbYesOqKgctsJG({8715,10395,11970,10605,10605,11550,7455,12285,11025}))
+local ToggleButton = Instance.new(bSOIWgCqoWuvMbYesOqKgctsJG({8820,10605,12600,12180,6930,12285,12180,12180,11655,11550}))
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild(bSOIWgCqoWuvMbYesOqKgctsJG({8400,11340,10185,12705,10605,11970,7455,12285,11025}))
 
 -- Personalización de la posición y tamaño del botón (puedes ajustar estos valores)
 ToggleButton.Size = UDim2.new(0, 250, 0, 60)  -- Ajusta el tamaño del botón (ancho, alto)
 ToggleButton.Position = UDim2.new(0, 10, 0, 10)  -- Ajusta la posición del botón (X, Y) desde la esquina superior izquierda
-ToggleButton.Text = "AIMBOT"  -- Texto del botón
+ToggleButton.Text = bSOIWgCqoWuvMbYesOqKgctsJG({6825,7665,8085,6930,8295,8820})  -- Texto del botón
 ToggleButton.Parent = ScreenGui
 ToggleButton.BackgroundColor3 = Color3.fromRGB(0, 255, 0)  -- Verde cuando está activado
 
@@ -344,10 +359,10 @@ ToggleButton.TextSize = 30  -- Ajusta este valor para cambiar el tamaño del tex
 ToggleButton.MouseButton1Click:Connect(function()
     AimbotEnabled = not AimbotEnabled
     if AimbotEnabled then
-        ToggleButton.Text = "OFF"  -- Cambia el texto cuando se activa
+        ToggleButton.Text = bSOIWgCqoWuvMbYesOqKgctsJG({8295,7350,7350})  -- Cambia el texto cuando se activa
         ToggleButton.BackgroundColor3 = Color3.fromRGB(255, 0, 0)  -- Rojo cuando está desactivado
     else
-        ToggleButton.Text = "ON"  -- Cambia el texto cuando se desactiva
+        ToggleButton.Text = bSOIWgCqoWuvMbYesOqKgctsJG({8295,8190})  -- Cambia el texto cuando se desactiva
         ToggleButton.BackgroundColor3 = Color3.fromRGB(0, 255, 0)  -- Verde cuando está activado
         CurrentTarget = nil  -- Limpiar objetivo cuando el aimbot se desactiva
     end
@@ -357,46 +372,46 @@ end)
 
 
 -- Inicialización del Menú
-lib:AddToggle("Activar Aimbot", function(state)
+lib:AddToggle(bSOIWgCqoWuvMbYesOqKgctsJG({6825,10395,12180,11025,12390,10185,11970,3360,6825,11025,11445,10290,11655,12180}), function(state)
     AimbotEnabled = state
     if not state then
         CurrentTarget = nil -- Limpiar objetivo al desactivar
     end
 end)
 
-lib:AddToggle("Activar FOV", function(state)
+lib:AddToggle(bSOIWgCqoWuvMbYesOqKgctsJG({6825,10395,12180,11025,12390,10185,11970,3360,7350,8295,9030}), function(state)
     FOVVisible = state
 end)
 
-lib:AddComboBox("Tamaño FOV", {"50", "100", "150", "200", "250"}, function(selection)
+lib:AddComboBox(bSOIWgCqoWuvMbYesOqKgctsJG({8820,10185,11445,10185,20475,18585,11655,3360,7350,8295,9030}), {bSOIWgCqoWuvMbYesOqKgctsJG({5565,5040}), bSOIWgCqoWuvMbYesOqKgctsJG({5145,5040,5040}), bSOIWgCqoWuvMbYesOqKgctsJG({5145,5565,5040}), bSOIWgCqoWuvMbYesOqKgctsJG({5250,5040,5040}), bSOIWgCqoWuvMbYesOqKgctsJG({5250,5565,5040})}, function(selection)
     FOVSize = tonumber(selection)
 end)
 
-lib:AddToggle("Hitbox", function(state)
+lib:AddToggle(bSOIWgCqoWuvMbYesOqKgctsJG({7560,11025,12180,10290,11655,12600}), function(state)
     hitboxActive = state
 end)
 
 -- ComboBox para seleccionar Tamaño de Hitbox
-lib:AddComboBox("Hitbox Size", {"1", "2", "3", "4", "5"}, function(selection)
-    if selection == "2" then
+lib:AddComboBox(bSOIWgCqoWuvMbYesOqKgctsJG({7560,11025,12180,10290,11655,12600,3360,8715,11025,12810,10605}), {bSOIWgCqoWuvMbYesOqKgctsJG({5145}), bSOIWgCqoWuvMbYesOqKgctsJG({5250}), bSOIWgCqoWuvMbYesOqKgctsJG({5355}), bSOIWgCqoWuvMbYesOqKgctsJG({5460}), bSOIWgCqoWuvMbYesOqKgctsJG({5565})}, function(selection)
+    if selection == bSOIWgCqoWuvMbYesOqKgctsJG({5250}) then
         activeHeadSize = Vector3.new(2, 2, 2)  
-    elseif selection == "3" then
+    elseif selection == bSOIWgCqoWuvMbYesOqKgctsJG({5355}) then
         activeHeadSize = Vector3.new(3, 3, 3)  
-    elseif selection == "1" then
+    elseif selection == bSOIWgCqoWuvMbYesOqKgctsJG({5145}) then
         activeHeadSize = Vector3.new(1, 1, 1)  
-    elseif selection == "4" then
+    elseif selection == bSOIWgCqoWuvMbYesOqKgctsJG({5460}) then
         activeHeadSize = Vector3.new(4, 4, 4) 
-    elseif selection == "5" then
+    elseif selection == bSOIWgCqoWuvMbYesOqKgctsJG({5565}) then
         activeHeadSize = Vector3.new(5, 5, 5)  
     end
 end)
 
 -- Dropdown para seleccionar Transparencia
-lib:AddComboBox("Hitbox Transparency", {"0", "0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1"}, function(selection)
+lib:AddComboBox(bSOIWgCqoWuvMbYesOqKgctsJG({7560,11025,12180,10290,11655,12600,3360,8820,11970,10185,11550,12075,11760,10185,11970,10605,11550,10395,12705}), {bSOIWgCqoWuvMbYesOqKgctsJG({5040}), bSOIWgCqoWuvMbYesOqKgctsJG({5040,4830,5145}), bSOIWgCqoWuvMbYesOqKgctsJG({5040,4830,5250}), bSOIWgCqoWuvMbYesOqKgctsJG({5040,4830,5355}), bSOIWgCqoWuvMbYesOqKgctsJG({5040,4830,5460}), bSOIWgCqoWuvMbYesOqKgctsJG({5040,4830,5565}), bSOIWgCqoWuvMbYesOqKgctsJG({5040,4830,5670}), bSOIWgCqoWuvMbYesOqKgctsJG({5040,4830,5775}), bSOIWgCqoWuvMbYesOqKgctsJG({5040,4830,5880}), bSOIWgCqoWuvMbYesOqKgctsJG({5040,4830,5985}), bSOIWgCqoWuvMbYesOqKgctsJG({5145})}, function(selection)
     hitboxTransparency = tonumber(selection) or 0.5  
 end)
 
-lib:AddToggle("Boxes", function(state)
+lib:AddToggle(bSOIWgCqoWuvMbYesOqKgctsJG({6930,11655,12600,10605,12075}), function(state)
     ESPEnabled = state
     if not state then
         for _, player in pairs(Players:GetPlayers()) do
@@ -413,7 +428,7 @@ lib:AddToggle("Boxes", function(state)
     end
 end)
 
-lib:AddToggle("Chams", function(state)
+lib:AddToggle(bSOIWgCqoWuvMbYesOqKgctsJG({7035,10920,10185,11445,12075}), function(state)
     ChamsActive = state
     if not state then
         for _, player in pairs(Players:GetPlayers()) do
@@ -424,7 +439,7 @@ lib:AddToggle("Chams", function(state)
     end
 end)
 
-lib:AddToggle("TeamCheck", function(state)
+lib:AddToggle(bSOIWgCqoWuvMbYesOqKgctsJG({8820,10605,10185,11445,7035,10920,10605,10395,11235}), function(state)
     TeamCheckEnabled = state
 end)
 
@@ -458,3 +473,4 @@ RunService.RenderStepped:Connect(function()
     end
 end)
 
+        
